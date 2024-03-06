a3313dac839f6ede7ea22d1224250d01374d297e
TheNobody-12 <sarthak.kce20@sot.pdpu.ac.in>
2024-03-06 15:23:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HelmetDetection",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    author="TheNobody-12",
+    author_email="sarthak.kce20@sot.pdpu.ac.in",
+    install_requires=[]
+)
